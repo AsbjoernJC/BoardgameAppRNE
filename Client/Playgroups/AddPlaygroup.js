e5803526f8cfa9bfa4639635fa18bf0e8ff6c785
unknown <asbjoernjc@gmail.com>
2022-11-01 07:22:36 +0100

    found how to store data

--- Client/Playgroups/AddPlaygroup.js
+++ Client/Playgroups/AddPlaygroup.js
@@ -118,6 +118,15 @@ class AddPlaygroup extends React.Component {
       //     // saving error
       //   }
       // }
+
+      // const getData = async () => {
+      //   try {
+      //     const jsonValue = await AsyncStorage.getItem('@storage_Key')
+      //     return jsonValue != null ? JSON.parse(jsonValue) : null;
+      //   } catch(e) {
+      //     // error reading value
+      //   }
+      // }
     }
   }
 
